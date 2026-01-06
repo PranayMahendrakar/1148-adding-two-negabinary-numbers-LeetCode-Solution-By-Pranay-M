10c60f1ec294d7edd2f22f960fee2569349ee206
Pranay Mahendrakar <mahendrakarpranay@gmail.com>
2026-01-06 11:38:54 +0530

    Initial commit

---
+++ 2026-01-02 15.39.37 - Accepted - runtime 1ms - memory 17.3MB.py
@@ -0,0 +1,24 @@
+class Solution:
+    def addNegabinary(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        result = []
+        carry = 0
+        i, j = len(arr1) - 1, len(arr2) - 1
+        
+        while i >= 0 or j >= 0 or carry:
+            if i >= 0:
+                carry += arr1[i]
+                i -= 1
+            if j >= 0:
+                carry += arr2[j]
+                j -= 1
+            
+            # In base -2, we have: carry = digit + (-2) * new_carry
+            # digit should be 0 or 1
+            result.append(carry & 1)  # carry % 2
+            carry = -(carry >> 1)     # -carry // 2
+        
+        # Remove leading zeros
+        while len(result) > 1 and result[-1] == 0:
+            result.pop()
+        
+        return result[::-1]
